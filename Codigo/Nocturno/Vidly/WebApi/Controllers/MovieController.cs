85da5dd0f7949f324bc7d257140ec13b82912a88
unknown <daniel18acevedo@gmail.com>
2020-09-09 20:34:21 -0300

    Subiendo codigo de la noche

---
+++ Codigo/Nocturno/Vidly/WebApi/Controllers/MovieController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi
+{
+    [ApiController]
+    [Route("api/movies")]
+    public class MovieController : ControllerBase
+    {
+        //api/movies
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        //api/movies/5
+        [HttpGet("{id}")]
+        public void Get([FromRoute]int id)
+        {
+
+        }
+
+        //api/movies?ageAllowed=5
+        [HttpGet]
+        public IActionResult GetBy([FromQuery]int ageAllowed)
+        {
+            return Ok();
+        }
+
+        //api/movies
+        [HttpPost]
+        public IActionResult Post([FromBody]object movie)
+        {
+            return Ok();
+        }
+
+        //api/movies/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromRoute]int id, [FromBody]object movie)
+        {
+            return Ok();
+        }
+
+        //api/movies/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute]int id)
+        {
+            return Ok();
+        }
+
+        //api/movies
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}

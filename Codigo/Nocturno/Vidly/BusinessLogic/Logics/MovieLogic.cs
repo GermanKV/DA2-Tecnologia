54f3b0bf8c68d589047c6ceb705a15e3a445215b
unknown <daniel18acevedo@gmail.com>
2020-09-13 01:54:34 -0300

    Subiendo inyeccion de dependencia

---
+++ Codigo/Nocturno/Vidly/BusinessLogic/Logics/MovieLogic.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogicInterface;
+using DataAccessInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic
+{
+    public class MovieLogic : IMovieLogic
+    {
+        private readonly IMovieRepository moviesRepository;
+
+        public MovieLogic(IMovieRepository moviesRepository)
+        {
+            this.moviesRepository = moviesRepository;
+        }
+    }
+}
---
+++ Codigo/Nocturno/Vidly/DataAccess/Repositories/MovieRepository.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessInterface;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly DbContext context;
+        private readonly DbSet<Movie> movies;
+
+        public MovieRepository(DbContext context)
+        {
+            this.context = context;
+            this.movies = context.Set<Movie>();
+        }
+    }
+}
